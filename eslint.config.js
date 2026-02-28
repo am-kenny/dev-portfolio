69c9a002fdf8ad4ff89b51ce387640d3d7c21a3c
Andrii Pr <a.v.prykhodko25@gmail.com>
2026-02-28 18:00:20 +0100

    chore: adjust eslint configuration, upgrade to version 9

--- eslint.config.js
+++ eslint.config.js
@@ -6,13 +6,13 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+  js.configs.recommended,
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -23,7 +23,20 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]', argsIgnorePattern: '^_' }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': ['error', { allowConstantExport: true }],
     },
+  },
+  // Context files export Provider + hook; allow non-component exports
+  {
+    files: ['src/context/**/*.jsx'],
+    rules: { 'react-refresh/only-export-components': 'off' },
+  },
+  // Config files run in Node (e.g. vite.config.js)
+  {
+    files: ['*.config.js', 'vite.config.js'],
+    languageOptions: { globals: { ...globals.node } },
   },
 ])
